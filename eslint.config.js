84dc06d5568162f5303fb452cd0f1227d1194e9e
odere-pro <165695298+odere-pro@users.noreply.github.com>
2025-03-10 14:52:47 +0100

    feat: add static code check

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: '19',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+    ignores: [
+      'node_modules',
+      'dist',
+      'dist-ssr',
+      '*.local',
+      '.yarn',
+      '.pnp.cjs',
+      '.pnp.loader.mjs',
+      '.yarnrc.yml',
+    ],
+  },
+];
