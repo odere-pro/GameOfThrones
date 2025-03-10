cbb29f5c62336cb65ddc08e9b3d4166dfd64f3e7
odere-pro <165695298+odere-pro@users.noreply.github.com>
2025-03-10 16:51:06 +0100

    feat: fetch all characters

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CACHE_KEY, fetchCharacters } from './routes/home';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
@@ -24,13 +26,22 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient();
+
+queryClient.prefetchQuery({
+  queryKey: [CACHE_KEY],
+  queryFn: fetchCharacters,
+});
+
 // Render the app
 const rootElement = document.getElementById('app');
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
+      <ReactQueryDevtools buttonPosition="top-right" />
       <TanStackRouterDevtools />
     </>
   ),
